0842d5b537dc27bf4c902809c04dc68299429457
Ardy <125114027+artace23@users.noreply.github.com>
2025-05-30 14:17:43 +0800

    Update supabaseBE.js

--- components/backend/supabaseBE.js
+++ components/backend/supabaseBE.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'URL_KEY';
-const SUPABASE_ANON_KEY = 'API_KEY';
+const SUPABASE_URL = 'URL_KEY__';
+const SUPABASE_ANON_KEY = 'API_KEY__';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
